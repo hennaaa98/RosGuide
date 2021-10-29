4063d359fc25a4003a142b2d4fecd71c8a499b0b
SeaBatt <sbattle11ses@gmail.com>
2021-10-29 06:58:34 +0200

    satchmo Popover functionality

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PopoverController } from '@ionic/angular'; //import the popover controller
 
 import { AboutKaffeKildenComponent } from '../components/about-kaffe-kilden/about-kaffe-kilden.component'; // import the component.
+import { AboutSatchmoComponent } from '../components/about-satchmo/about-satchmo.component';
 
 
 @Component({
@@ -20,17 +21,23 @@ export class Tab2Page {
   ngOnInit(){
   } 
 
-  openPopover(){
-    console.log("Pop over activated")
-  }
 
-   // User clicked on 'About this app' * And add the asynch clicked method.
-   async aboutKaffeKilden() {
+  // User clicked on 'About this app' * And add the asynch clicked method.
+  async aboutKaffeKilden() {
     const popover = await this.popoverController.create({ //first we create the popover, 
       component: AboutKaffeKildenComponent, //it expected a component property, we use our kaffeKilden
       translucent: true // state we want it to be translucent.
     });
     await popover.present(); //now it's ready to be called.
+  }
+
+   
+  async aboutSatchmo() {
+    const popover = await this.popoverController.create({ 
+      component: AboutSatchmoComponent,
+      translucent: true 
+    });
+    await popover.present(); 
   }
 
 
