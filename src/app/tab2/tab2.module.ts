fde2a592a853cefb9423e4d86c6ee2add0aecaea
SeaBatt <sbattle11ses@gmail.com>
2021-10-29 06:17:24 +0200

    kaffekildePopoverFunctionality

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,21 +1,24 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+import { AboutKaffeKildenComponent } from '../components/about-kaffe-kilden/about-kaffe-kilden.component'; //Import Component.
+
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
+
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule 
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page, AboutKaffeKildenComponent], // declare the component
+  entryComponents: [AboutKaffeKildenComponent] // as well as declaring entry component.
 })
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular'; //import the popover controller
+
+import { AboutKaffeKildenComponent } from '../components/about-kaffe-kilden/about-kaffe-kilden.component'; // import the component.
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,27 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  //We inject the popover controller to the constructor 
+  constructor(public popoverController: PopoverController) {
+
+  }
+
+
+  ngOnInit(){
+  } 
+
+  openPopover(){
+    console.log("Pop over activated")
+  }
+
+   // User clicked on 'About this app' * And add the asynch clicked method.
+   async aboutKaffeKilden() {
+    const popover = await this.popoverController.create({ //first we create the popover, 
+      component: AboutKaffeKildenComponent, //it expected a component property, we use our kaffeKilden
+      translucent: true // state we want it to be translucent.
+    });
+    await popover.present(); //now it's ready to be called.
+  }
+
 
 }
